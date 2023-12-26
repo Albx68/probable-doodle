c0c1cddd0e06324450a4e202a83863495c711eb3
Albert Yumnam <84399334+Albx68@users.noreply.github.com>
2023-12-26 22:46:33 +0530

    added constants for face color

--- src/components/AlbyIllus/AlbyIllus.tsx
+++ src/components/AlbyIllus/AlbyIllus.tsx
@@ -1,3 +1,4 @@
+import { colors } from '@/utils/constants/colors'
 import { motion } from 'framer-motion'
 import React from 'react'
 
@@ -8,14 +9,15 @@ type TAlbyIllus = {
 
 const AlbyIllus = ({ canvasHeight = 400, canvasWidth = 400 }: TAlbyIllus) => {
     const faceCircle = ""
+    const facePath = `M50 10 
+    C 77.614 10 90 22.386 90 50 
+    C 90 77.614 77.614 90 50 90 
+    C 22.386 90 10 77.614 10 50 
+    C 10 22.386 22.386 10 50 10`
     return (
         <div>
-            <svg width={canvasWidth} height={canvasHeight}>
-                <path d="M50 10 
-           C 77.614 10 90 22.386 90 50 
-           C 90 77.614 77.614 90 50 90 
-           C 22.386 90 10 77.614 10 50 
-           C 10 22.386 22.386 10 50 10" fill="red" stroke="black" />
+            <svg width={canvasWidth} height={canvasHeight} >
+                <motion.path d={facePath} fill={colors["face"]} stroke="black" />
             </svg>
         </div>
     )
