613ffcede1e72e4103c7c870156c70b6c71acb98
Albert Yumnam <84399334+Albx68@users.noreply.github.com>
2023-12-26 22:42:52 +0530

    change path to cubic bezier

--- src/components/AlbyIllus/AlbyIllus.tsx
+++ src/components/AlbyIllus/AlbyIllus.tsx
@@ -11,7 +11,11 @@ const AlbyIllus = ({ canvasHeight = 400, canvasWidth = 400 }: TAlbyIllus) => {
     return (
         <div>
             <svg width={canvasWidth} height={canvasHeight}>
-                <path d="M50 10 A 40 40 0 1 0 50 90 A 40 40 0 1 0 50 10" fill="red" stroke="black" />
+                <path d="M50 10 
+           C 77.614 10 90 22.386 90 50 
+           C 90 77.614 77.614 90 50 90 
+           C 22.386 90 10 77.614 10 50 
+           C 10 22.386 22.386 10 50 10" fill="red" stroke="black" />
             </svg>
         </div>
     )
