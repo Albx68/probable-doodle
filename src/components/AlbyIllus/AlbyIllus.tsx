33e81ab47e6b88d4863344ded96f5018a826d1de
Albert Yumnam <84399334+Albx68@users.noreply.github.com>
2023-12-27 00:05:38 +0530

    improved getFacePath

--- src/components/AlbyIllus/AlbyIllus.tsx
+++ src/components/AlbyIllus/AlbyIllus.tsx
@@ -15,7 +15,7 @@ const AlbyIllus = ({ canvasHeight = 300, canvasWidth = 300 }: TAlbyIllus) => {
     const cy = canvasHeight / 2
 
 
-    const facePath = getFacePath(canvasWidth, canvasHeight, 2)
+    const facePath = getFacePath(canvasWidth, canvasHeight, 1.01)
     return (
         <div>
             <svg width={canvasWidth} height={canvasHeight} style={{ backgroundColor: "#333" }}>
@@ -37,47 +37,34 @@ const getFacePath = (canvasWidth: number, canvasHeight: number, curveFactor: num
     const p3 = vec(canvasWidth, cy)
     const p4 = vec(cx, canvasHeight)
 
-    const quarterWidth = canvasWidth / 4
-    const quarterHeight = canvasHeight / 4
+    const quarterWidth = canvasWidth / 3.5
+    const quarterHeight = canvasHeight / 3.5
 
-    const handle1x = 0
-    const handle1y = quarterHeight
+    const handle1x = 0;
+    const handle1y = cy - quarterHeight * curveFactor;
 
-    const handle2x = quarterWidth
-    const handle2y = 0
+    const handle2x = cx - quarterWidth * curveFactor;
+    const handle2y = 0;
 
-    const handle3x = quarterWidth * 3
-    const handle3y = 0
+    const handle3x = cx + quarterWidth * curveFactor;
+    const handle3y = 0;
 
-    const handle4x = canvasWidth
-    const handle4y = quarterHeight
+    const handle4x = canvasWidth;
+    const handle4y = cy - quarterHeight * curveFactor;
 
-    const handle5x = canvasWidth
-    const handle5y = quarterHeight * 3
+    const handle5x = canvasWidth;
+    const handle5y = cy + quarterHeight * curveFactor;
 
-    const handle6x = quarterWidth * 3
-    const handle6y = canvasHeight
+    const handle6x = cx + quarterWidth * curveFactor;
+    const handle6y = canvasHeight;
 
-    const handle7x = quarterWidth
-    const handle7y = canvasHeight
+    const handle7x = cx - quarterWidth * curveFactor;
+    const handle7y = canvasHeight;
 
-    const handle8x = 0
-    const handle8y = quarterHeight * 3
+    const handle8x = 0;
+    const handle8y = cy + quarterHeight * curveFactor;
 
-    // const handles = [];
 
-    // for (let i = 0; i < 8; i++) {
-    //     const angle = (i * Math.PI) / 4; // Divide the circle into 8 angles
-
-    //     // Define the radius for the handles
-    //     const radius = i % 2 === 0 ? canvasWidth : canvasHeight;
-
-    //     // Calculate the coordinates using polar to Cartesian conversion
-    //     const handleX = canvasWidth / 2 + radius * Math.cos(angle);
-    //     const handleY = canvasHeight / 2 + radius * Math.sin(angle);
-
-    //     handles.push(vec(handleX, handleY));
-    // }
 
     const handle1 = vec(handle1x, handle1y);
     const handle2 = vec(handle2x, handle2y);
@@ -93,8 +80,7 @@ const getFacePath = (canvasWidth: number, canvasHeight: number, curveFactor: num
     C${handle3.x} ${handle3.y} ${handle4.x} ${handle4.y} ${p3.x} ${p3.y}
     C${handle5.x} ${handle5.y} ${handle6.x} ${handle6.y} ${p4.x} ${p4.y}
     C${handle7.x} ${handle7.y} ${handle8.x} ${handle8.y} ${p1.x} ${p1.y}
-
-
+    Z
     `
 }
 
